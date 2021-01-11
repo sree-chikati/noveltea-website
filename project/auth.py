164dc17beaabc852c5f6e3f5eb4aff2ddd12abcf
sree-chikati <srechi453.fusdk12@gmail.com>
2021-01-10 22:41:05 -0800

    adding missed parts

--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required
+from flask_login import login_user
+from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
--- project/models.py
+++ project/models.py
@@ -1,3 +1,4 @@
+from flask_login import UserMixin
 from . import db
 
 class User(UserMixin, db.Model):
